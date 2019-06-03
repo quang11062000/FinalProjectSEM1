5a0c801d45873b53fc7445b6cd0ce61970c4b628
quang11062000 <quangnq.nde18082@vtc.edu.vn>
2019-06-03 14:23:48 +0700

    message

--- PL/Program.cs
+++ PL/Program.cs
@@ -9,15 +9,9 @@ namespace PL
     {
         static void Main(string[] args)
         {
-        //    Menus m = new Menus();
-        //     m.Menuchoice(null);
-                ScheduleBL schedu = new ScheduleBL();
-                List<Schedule> list = schedu.DisplaySchedule();
-                Console.WriteLine("|{0,-10}|{1,-30}|{2,-20}|{3,-20}|{4,-20}|","Match_id","Match_name","Match_day","Match_time","Stadium");
-                foreach (var item1 in list)
-                {
-                    Console.WriteLine("|{0,-10}|{1,-30}|{2,-20}|{3,-20}|{4,-20}|", item1.Match_id, string.Concat(item1.Home_team,' ',"vs",' ',item1.Away_team),item1.Match_day,item1.Match_time,item1.Stadium_name);
-                }
+           Menus m = new Menus();
+            m.Menuchoice(null);
+              
         }   
     }
 }
